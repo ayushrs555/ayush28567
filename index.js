97b06fc58e83cc38b6454db2e04ff17a80037b11
ayushrs555 <ayushrsingh5151@gmail.com>
2025-02-22 20:40:39 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+<script>
+
+var test = new Date();
+if (test.constructor == Array)
+  {
+  document.write("This is an Array");
+  }
+if (test.constructor == Boolean)
+  {
+  document.write("This is a Boolean");
+  }
+if (test.constructor == Date)
+  {
+  document.write("This is a Date");
+  }
+if (test.constructor == String)
+  {
+  document.write("This is a String");
+  }
+</script>
